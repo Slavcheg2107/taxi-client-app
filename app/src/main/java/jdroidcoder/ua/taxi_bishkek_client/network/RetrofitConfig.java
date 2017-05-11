55b34c1546f647035f4fdf2f17762cb6d19dbb49
jdroidcoder <anonunfeeling1@gmail.com>
2017-05-11 19:40:48 +0300

    fix

--- app/src/main/java/jdroidcoder/ua/taxi_bishkek_client/network/RetrofitConfig.java
+++ app/src/main/java/jdroidcoder/ua/taxi_bishkek_client/network/RetrofitConfig.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by jdroidcoder on 07.04.17.
  */
 public class RetrofitConfig {
-    private final static String BASE_URL = "http://192.168.1.103:4547/";
+    private final static String BASE_URL = "http://104.236.30.198:4849/";
     private Retrofit retrofit = new Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BASE_URL)
